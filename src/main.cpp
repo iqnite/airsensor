10530dcf8ef5e83235746543826248e28327c8ac
iqnite <iqnitetechnologies@gmail.com>
2025-06-04 11:40:09 +0200

    Improve visualization

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
-#define VERSION "0.1.0-development.1m"
+#define VERSION "0.1.0-development.2"
 
 #define SEA_LEVEL_PRESSURE_HPA 1013.25
 #define STATUS_LED 8
+#define DEGREE "" // 0xB0
+#define START_ROW 30
+#define NAME_COL 0
+#define VALUE_COL 50
+#define UNIT_COL 95
 
 #include <Arduino.h>
 #include <Adafruit_BME280.h>
@@ -23,22 +28,23 @@ void setup()
     ;
   pinMode(STATUS_LED, OUTPUT);
   u8g2.begin();
-  u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_ncenB08_tr);
   u8g2.setFontMode(1); // Enable font mode for text rendering
-  u8g2.setCursor(0, 10);
-  u8g2.print("HTL AirSensor ");
-  u8g2.print(VERSION);
-  u8g2.sendBuffer();
 }
 
 void loop()
 {
   static bool bmeConnected = false;
 
-  delay(100);
-
   toggle(STATUS_LED);
+
+  u8g2.clearBuffer();
+
+  u8g2.setCursor(0, 10);
+  u8g2.print("HTL AirSensor ");
+  u8g2.print(VERSION);
+
+  delay(2000);
 
   if (!bmeConnected)
   {
@@ -52,18 +58,58 @@ void loop()
   const float humidity = bme.readHumidity();
   const float altitude = bme.readAltitude(SEA_LEVEL_PRESSURE_HPA);
 
+  Serial.println();
+
   Serial.print("Temperature \t");
   Serial.print(temperature);
-  Serial.println("\t °C");
-  Serial.print("Pressure \t");
-  Serial.print(pressure / 100.0F);
-  Serial.println("\t hPa");
+  Serial.print("\t ");
+  Serial.print(DEGREE);
+  Serial.println("C");
+
   Serial.print("Humidity \t");
   Serial.print(humidity);
   Serial.println("\t %");
+
+  Serial.print("Pressure \t");
+  Serial.print(pressure / 100.0F);
+  Serial.println("\t hPa");
+
   Serial.print("Altitude \t");
   Serial.print(altitude);
   Serial.println("\t m");
+
+  // Display the data on the display
+
+  u8g2.setCursor(NAME_COL, START_ROW);
+  u8g2.print("TEMP:");
+  u8g2.setCursor(VALUE_COL, START_ROW);
+  u8g2.print(temperature);
+  u8g2.setCursor(UNIT_COL, START_ROW);
+  u8g2.print(DEGREE);
+  u8g2.print("C");
+
+  u8g2.setCursor(NAME_COL, START_ROW + 10);
+  u8g2.print("HUM:");
+  u8g2.setCursor(VALUE_COL, START_ROW + 10);
+  u8g2.print(humidity);
+  u8g2.setCursor(UNIT_COL, START_ROW + 10);
+  u8g2.print("%");
+
+  u8g2.setCursor(NAME_COL, START_ROW + 20);
+  u8g2.print("PRES:");
+  u8g2.setCursor(VALUE_COL, START_ROW + 20);
+  u8g2.print(pressure / 100.0F);
+  u8g2.setCursor(UNIT_COL, START_ROW + 20);
+  u8g2.print("hPa");
+
+  u8g2.setCursor(NAME_COL, START_ROW + 30);
+  u8g2.print("ALT:");
+  u8g2.setCursor(VALUE_COL, START_ROW + 30);
+  u8g2.print(altitude);
+  u8g2.setCursor(UNIT_COL, START_ROW + 30);
+  u8g2.print("m");
+
+  u8g2.sendBuffer();
 }
 
 void spinner()
